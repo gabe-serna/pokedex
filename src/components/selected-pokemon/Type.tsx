8868a1076af63fa0bd71871a0612e7e17bf26808
Gabe Serna <gabriel.serna118@gmail.com>
2024-08-06 15:29:30 -0400

    Add Black Outline to Style Text

--- src/components/selected-pokemon/Type.tsx
+++ src/components/selected-pokemon/Type.tsx
@@ -8,7 +8,7 @@ const Type = ({ children }: Props) => {
   const color = types.get(children) as string;
   return (
     <div
-      className={`inline-block py-1 px-2 mr-1 mb-2 lg:text-xs sm:text-[10px]/3 font-semibold text-primary-foreground rounded-full border border-black bg-gradient-to-t from-[${color}] from-0% via-[hsl(from_${color}_h_s_calc(l_*_0.5))] to-[${color}] to-100% `}
+      className={`inline-block py-1 px-2 mr-1 mb-2 lg:text-xs sm:text-[10px]/3 font-semibold text-primary-foreground rounded-full border border-black bg-gradient-to-t from-[${color}] from-0% via-[hsl(from_${color}_h_s_calc(l_*_0.5))] to-[${color}] to-100% text-outline-black `}
     >
       {children.toUpperCase()}
     </div>
